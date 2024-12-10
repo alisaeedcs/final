e80d43bebb279b7f76de36a903e566dfe1eac39e
Ali Saeed <asaeed5637@gmail.com>
2024-12-09 19:32:57 -0800

    update 7

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <deque>
+#include <vector>
 using namespace std;
 
 const int ROUNDS = 10;
@@ -18,6 +19,13 @@ struct Muffin {
     string muffinType;
 
     Muffin(string n, string m) : name(n), muffinType(m) {}
+};
+
+struct Bracelet {
+    string name;
+    string color;
+
+    Bracelet(string n, string c) : name(n), color(c) {}
 };
 
 //all for coffee under
@@ -47,24 +55,33 @@ int main() {
     //muffin queue
     deque<Muffin> muffinQueue;
     
-    //initialize the coffee queue (3 customers)
+    //initialize the coffee queue (3 customers) also the muffin
     for (int i = 0; i < 3; i++) {
         addCustomer(head, names[rand() % 12], drinkOrders[rand() % 7]);
+        addMuffinCustomer(muffinQueue, names[rand() % 12], muffins[rand() % 5]);
     }
 
-    cout << "Pre-Simulation beginning:\n";
-    outputQueue(head);
-    //start simulation
     for (int i = 1; i <= ROUNDS; i++) {
         cout << "Round " << i << ":\n";
-        
-        serveCustomer(head);
 
+        //coffee booth
+        cout << "COFFEE BOOTH:\n";
+        serveCustomer(head);
         //50 percent of adding a new customer using integer to deterine bool
         if (rand() % 2) {
             addCustomer(head, names[rand() % 12], drinkOrders[rand() % 7]);
         }
         outputQueue(head);
+
+        //muffin booth
+        cout << "MUFFIN BOOTH:\n";
+        serveMuffinCustomer(muffinQueue);
+        if (rand() % 2) {
+            addMuffinCustomer(muffinQueue, names[rand() % 12], muffins[rand() % 5]);
+        }
+        outputMuffin(muffinQueue);
+
+
     }
 
     return 0;
@@ -72,7 +89,7 @@ int main() {
 
 void addCustomer(Customer*& head, string name, string order) {
     Customer* newCustomer = new Customer(name, order);
-    cout << "\tAdded " << newCustomer->name << " to the queue!\n";
+    cout << "\tAdded " << newCustomer->name << " to the coffee queue!\n";
     if (!head) {
         head = newCustomer;
     }
@@ -87,7 +104,7 @@ void addCustomer(Customer*& head, string name, string order) {
 
 void serveCustomer(Customer*& head) {
     if (!head) {
-        cout << "\tNo customers in queue\n";
+        cout << "\tNo customers in coffee queue\n";
         return;
     }
     Customer* temp = head;
@@ -98,7 +115,7 @@ void serveCustomer(Customer*& head) {
 
 void outputQueue(Customer* head) {
     if (!head) {
-        cout << "\tThe queue is empty\n";
+        cout << "\tThe coffee queue is empty\n";
         return;
     }
     Customer* temp = head;
@@ -114,7 +131,27 @@ void outputQueue(Customer* head) {
 
 void addMuffinCustomer(deque<Muffin>& queue, string name, string order) {
     queue.push_back(Muffin(name, order));
-    cout << "\t\tAdded " << name << " to the muffin queue ordering a " << order << " muffin\n";
+    cout << "\tAdded " << name << " to the muffin queue ordering a " << order << " muffin\n";
 }
-void serveMuffinCustomer(deque<Muffin>&);
-void outputMuffin(deque<Muffin>&);
+void serveMuffinCustomer(deque<Muffin>& queue) {
+    if (queue.empty()) {
+        cout << "\tMuffin queue is empty.\n";
+        return;
+    }
+    Muffin temp = queue.front();
+    cout << "\tServing " << temp.name << " a " << temp.muffinType << " muffin.\n";
+    queue.pop_front();
+}
+void outputMuffin(deque<Muffin>& queue) {
+    if (queue.empty()) {
+        cout << "\tMuffin queue is empty.\n";
+        return;
+    }
+    cout << "\tMuffin queue:\n";
+    int count = 1;
+    for (auto& customer: queue) {
+        cout << "\t\t " << count << ". " << customer.name << " ordered a " << customer.muffinType << " muffin.\n";
+        count++;
+    }
+ 
+}
