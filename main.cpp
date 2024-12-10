f861d4460b4af270dd1f13ecf07bb87191010eb5
Ali Saeed <asaeed5637@gmail.com>
2024-12-09 18:52:49 -0800

    update 3

--- main.cpp
+++ main.cpp
@@ -4,21 +4,68 @@ using namespace std;
 const int ROUNDS = 10;
 
 //node for coffee sim
-struct customer {
+struct Customer {
     string name;
     string order;
-    customer* next;
+    Customer* next;
 
-    customer(string n, string o) : name(n), order(o), next(nullptr) {}
+    Customer(string n, string o) : name(n), order(o), next(nullptr) {}
 };
 
 //add customer function
-void add
+void addCustomer(Customer*&, string, string);
+
+//serve/delete customer function
+void serveCustomer(Customer*&);
+
+//output the queue
+void outputQueue(Customer*&);
 
 int main() {
     //names for coffee and also drink orders, mayb used for more in future
     string names[] = {"Mo", "Ali", "Omar", "Hannah", "Ben", "Sahil", "Najla", "Hanif", "Tom", "Nick", "Elizabeth", "Sarah"};
     string drinkOrders[] = {"Espresso", "Americano", "Latte", "Mocha", "Cappucino", "Cold Brew", "Chai"};
 
+    Customer* head = nullptr;
     
+    //initialize the queue
+
 }
+
+void addCustomer(Customer*& head, string name, string order) {
+    Customer* newCustomer = new Customer(name, order);
+    if (!head) {
+        head = newCustomer;
+    }
+    else {
+        Customer* temp = head;
+        while(temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newCustomer;
+    }
+}
+
+void serveCustomer(Customer*& head) {
+    if (!head) {
+        cout << "No customers in queue\n";
+        return;
+    }
+    Customer* temp = head;
+    cout << "Serving " << temp->name << " a " << temp->order << ".\n";
+    head = head->next;
+    delete temp;
+}
+
+void outputQueue(Customer*& head) {
+    if (!head) {
+        cout << "The queue is empty\n";
+        return;
+    }
+    cout << "Coffee queue:\n";
+    int count = 1;
+    while (head) {
+        cout << "\t" << count << ". " << head->name << ". Order:"
+        count++;
+    }
+} 
