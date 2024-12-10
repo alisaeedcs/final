e25a02f1e4be426571f395a0de0ff2849078be65
Ali Saeed <asaeed5637@gmail.com>
2024-12-09 19:02:51 -0800

    update 4

--- main.cpp
+++ main.cpp
@@ -19,17 +19,29 @@ void addCustomer(Customer*&, string, string);
 void serveCustomer(Customer*&);
 
 //output the queue
-void outputQueue(Customer*&);
+void outputQueue(Customer*);
 
 int main() {
+    srand(time(0));
     //names for coffee and also drink orders, mayb used for more in future
     string names[] = {"Mo", "Ali", "Omar", "Hannah", "Ben", "Sahil", "Najla", "Hanif", "Tom", "Nick", "Elizabeth", "Sarah"};
     string drinkOrders[] = {"Espresso", "Americano", "Latte", "Mocha", "Cappucino", "Cold Brew", "Chai"};
 
     Customer* head = nullptr;
     
-    //initialize the queue
+    //initialize the queue (3 customers)
+    for (int i = 0; i < 3; i++) {
+        addCustomer(head, names[rand() % 12], drinkOrders[rand() % 7]);
+    }
 
+    cout << "Pre-Simulation beginning:\n";
+    outputQueue(head);
+    //start simulation
+    for (int i = 1; i <= ROUNDS; i++) {
+        cout << "Round " << i << ":\n";
+    }
+
+    return 0;
 }
 
 void addCustomer(Customer*& head, string name, string order) {
@@ -57,15 +69,16 @@ void serveCustomer(Customer*& head) {
     delete temp;
 }
 
-void outputQueue(Customer*& head) {
+void outputQueue(Customer* head) {
     if (!head) {
         cout << "The queue is empty\n";
         return;
     }
+    Customer* temp = head;
     cout << "Coffee queue:\n";
     int count = 1;
-    while (head) {
-        cout << "\t" << count << ". " << head->name << ". Order:"
+    while (temp) {
+        cout << "\t" << count << ". " << temp->name << ". Order: " << temp->order << endl;
         count++;
     }
 } 
