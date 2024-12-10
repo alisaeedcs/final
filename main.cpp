e87984de76010ee8e374eaeedf5628359dbf5208
Ali Saeed <asaeed5637@gmail.com>
2024-12-09 18:42:52 -0800

    update 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+const int ROUNDS = 10;
+
+//node for coffee sim
+struct customer {
+    string name;
+    string order;
+    customer* next;
+
+    customer(string n, string o) : name(n), order(o), next(nullptr) {}
+};
+
+//add customer function
+void add
+
 int main() {
+    //names for coffee and also drink orders, mayb used for more in future
+    string names[] = {"Mo", "Ali", "Omar", "Hannah", "Ben", "Sahil", "Najla", "Hanif", "Tom", "Nick", "Elizabeth", "Sarah"};
+    string drinkOrders[] = {"Espresso", "Americano", "Latte", "Mocha", "Cappucino", "Cold Brew", "Chai"};
+
     
 }
